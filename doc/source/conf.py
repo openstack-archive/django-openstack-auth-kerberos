1c774adc6183296d90e17588011c7cce42b4dd63
lin-hua-cheng <os.lcheng@gmail.com>
2015-05-12 15:38:00 -0700

    Fix pep8 and doc error
    Change-Id: I0a0947d7b58b870eddb08c8584927a98e8637dee

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'openstack_auth_kerberos.tests.settings'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
